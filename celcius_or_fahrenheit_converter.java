c1b57c907e0565aeefd381d4d75b2f743bbbd387
achafine <achafine@gmail.com>
2024-11-15 15:37:51 +0100

    Update celcius_or_fahrenheit_converter.java
    On verifie les entrées utilisateur et on revoie le format du resultat après calcul

--- celcius_or_fahrenheit_converter.java
+++ celcius_or_fahrenheit_converter.java
@@ -15,31 +15,51 @@ public class TP{
 
 
       choix = sc.nextLine().charAt(0);
+      //On quitte le programme en laissant un message
       if (choix == '3') {
         System.out.println("Merci d'avoir utilisé notre programme de Conversion !");
         break;
-      }else if (choix == '1') {
-        System.out.println("Vous avez choisi la Conversion de Celcius vers Fahrenheit");
-        System.out.println("Température à convertir : ");
-        float temp = 0.0f;
-        float tempConver = 0.0f;
-        temp = sc.nextFloat();
-        sc.nextLine();
-        tempConver = 32.0f + ((9.0f/5.0f) * temp);
-        System.out.println(temp+" correspond à : "+tempConver);
-      }else if (choix == '2') {
-        float temp = 0.0f;
-        float tempConver = 0.0f;
-        System.out.println("Vous avez choisi la Conversion de Fahrenheit vers Celcius");
-        temp = sc.nextFloat();
-        sc.nextLine();
-        tempConver = 32.0f + ((9.0f/5.0f) * temp);
-        tempConver = ((temp - 32.0f) * 5.0f) / 9.0f;
-        System.out.println(temp+" correspond à : "+tempConver);
+      }
+      //on effectue les conversions
+      else if (choix == '1') {
+          System.out.println("Vous avez choisi la Conversion de Celcius vers Fahrenheit");
+          System.out.print("Température à convertir : ");
+
+        //On verifie l'entrée utilisateur
+        if (sc.hasNextFloat()) {
+          float temp = 0.0f;
+          float tempConver = 0.0f;
+          temp = sc.nextFloat();
+          sc.nextLine();
+          tempConver = 32.0f + ((9.0f/5.0f) * temp);
+          System.out.printf("%.2f°C correspond à : %.2f°F", temp, tempConver);
+        }else{
+          System.out.println("Veuillez entrer un nombre : ");
+          sc.nextLine();
+        }
+      }
+		
+      else if (choix == '2') {
+          System.out.println("Vous avez choisi la Conversion de Fahrenheit vers Celcius");
+          System.out.print("Température à convertir : ");
+          
+      //On verifie l'entrée utilisateur
+        if (sc.hasNextFloat()) {
+          float temp = 0.0f;
+          float tempConver = 0.0f;
+          temp = sc.nextFloat();
+          sc.nextLine();
+          tempConver = 32.0f + ((9.0f/5.0f) * temp);
+          tempConver = ((temp - 32.0f) * 5.0f) / 9.0f;
+          System.out.printf("%.2f°F correspond à : %.2f°C\n", temp, tempConver);
+        }else{
+          System.out.println("Veuillez entrer un nombre : ");
+          sc.nextLine();
+        }
       }else{
-        System.out.println("Choix invalide !!");
-        break;
+        System.out.println("Choix invalide veuillez essayez à nouveau!");
       }
     }
+      sc.close();
   }
 }
